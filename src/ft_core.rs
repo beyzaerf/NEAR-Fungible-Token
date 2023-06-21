88200a93628cec5195cbbfdf72fad395e03a2925
beyzerf <beyzaerfidan@gmail.com>
2023-06-21 15:26:39 +0300

    finish ft

--- src/ft_core.rs
+++ src/ft_core.rs
@@ -42,6 +42,7 @@ pub trait FungibleTokenCore {
     /// - `msg` - a string message that will be passed to `ft_on_transfer` contract call.
     ///
     /// Returns a promise which will result in the amount of tokens withdrawn from sender's account.
+    
     fn ft_transfer_call(
         &mut self,
         receiver_id: AccountId,
@@ -61,9 +62,10 @@ pub trait FungibleTokenCore {
 impl FungibleTokenCore for Contract {
     #[payable]
     fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>) {
-        /*
-            FILL THIS IN
-        */
+        assert_one_yocto();
+        let sender_id = env::predecessor_account_id();
+        let amount: Balance = amount.into();
+        self.internal_transfer(&sender_id, &receiver_id, amount, memo);
     }
 
     #[payable]
@@ -74,24 +76,29 @@ impl FungibleTokenCore for Contract {
         memo: Option<String>,
         msg: String,
     ) -> PromiseOrValue<U128> {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        assert_one_yocto();
+        let sender_id = env::predecessor_account_id();
+        let amount: Balance = amount.into();
+        self.internal_transfer(&sender_id, &receiver_id, amount, memo);
+        ext_ft_receiver::ext(receiver_id.clone())
+        .with_static_gas(GAS_FOR_FT_TRANSFER_CALL)
+        .ft_on_transfer(sender_id.clone(), amount.into(), msg)
+        .then(
+            Self::ext(env::current_account_id())
+                .with_static_gas(GAS_FOR_RESOLVE_TRANSFER)
+                .ft_resolve_transfer(&sender_id, receiver_id, amount.into()),
+        )
+        .into()
     }
 
     fn ft_total_supply(&self) -> U128 {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        // Return the total supply casted to a U128
+        self.total_supply.into()
     }
 
     fn ft_balance_of(&self, account_id: AccountId) -> U128 {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        // Return the balance of the account casted to a U128
+        self.accounts.get(&account_id).unwrap_or(0).into()
     }
 }
 
@@ -153,15 +160,51 @@ impl Contract {
     // 80, `ft_on_transfer` will resolve with `"20"`, and `ft_resolve_transfer`
     // will return `"80"`.
     #[private]
-    pub fn ft_resolve_transfer(
-        &mut self,
-        sender_id: &AccountId,
-        receiver_id: AccountId,
-        amount: U128,
-    ) -> U128 {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+pub fn ft_resolve_transfer(
+    &mut self,
+    sender_id: &AccountId,
+    receiver_id: AccountId,
+    amount: U128,
+) -> U128 {
+    let amount: Balance = amount.into();
+
+    // Get the unused amount from the `ft_on_transfer` call result.
+    let unused_amount = match env::promise_result(0) {
+        PromiseResult::NotReady => env::abort(),
+        // If the promise was successful, get the return value and cast it to a U128.
+        PromiseResult::Successful(value) => {
+            // If we can properly parse the value, the unused amount is equal to whatever is smaller - the unused amount or the original amount (to prevent malicious contracts)
+            if let Ok(unused_amount) = near_sdk::serde_json::from_slice::<U128>(&value) {
+                std::cmp::min(amount, unused_amount.0)
+            // If we can't properly parse the value, the original amount is returned.
+            } else {
+                amount
+            }
+        }
+        // If the promise wasn't successful, return the original amount.
+        PromiseResult::Failed => amount,
+    };
+
+    // If there is some unused amount, we should refund the sender
+    if unused_amount > 0 {
+        // Get the receiver's balance. We can only refund the sender if the receiver has enough balance.
+        let receiver_balance = self.accounts.get(&receiver_id).unwrap_or(0);
+        if receiver_balance > 0 {
+            // The amount to refund is the smaller of the unused amount and the receiver's balance as we can only refund up to what the receiver currently has.
+            let refund_amount = std::cmp::min(receiver_balance, unused_amount);
+            
+            // Refund the sender for the unused amount.
+            self.internal_transfer(&receiver_id, &sender_id, refund_amount, Some("Refund".to_string()));
+            
+            // Return what was actually used (the amount sent - refund)
+            let used_amount = amount
+                .checked_sub(refund_amount)
+                .unwrap_or_else(|| env::panic_str("Total supply overflow"));
+            return used_amount.into();
+        }
     }
+
+    // If the unused amount is 0, return the original amount.
+    amount.into()
+}
 }
